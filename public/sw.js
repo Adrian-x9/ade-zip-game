b5eea5f77eaf2ffcc9c27b1e8f96e6752200ce6d
Adrian-x9 <adrian.ulbrych@gmail.com>
2026-05-12 05:44:13 +0200

    fixes for v1.1

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'zip-game-cache-v1.2.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // Vite automatycznie dołączy haszowane nazwy plików JS/CSS w środowisku produkcyjnym
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
